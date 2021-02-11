311c7dcf80c6c9d2594c892f472050534b75cce1
Miotis2004 <miotis2004@gmail.com>
2021-02-11 05:41:50 -0800

    More refactoring

--- ArtistInformation/ArtistInformation/GetData.swift
+++ ArtistInformation/ArtistInformation/GetData.swift
@@ -44,31 +44,5 @@ class GetData: ObservableObject {
         }.resume()
     }
     
-    func getData (name: String) {
-        let url = URL(string: "https://itunes.apple.com/search?term={\(name)}")!
-        
-        let session = URLSession(configuration: .default)
-        
-        session.dataTask(with: url) { (data, _, error) in
-            if error != nil {
-                print((error?.localizedDescription)!)
-                return
-            }
-            
-            let json = try! JSON(data: data!)
-            
-            for i in json["results"] {
-                let artistName = i.1["artistName"].stringValue
-                let trackName = i.1["trackName"].stringValue
-                let releaseDate = i.1["releaseDate"].stringValue
-                let primaryGenreName = i.1["primaryGenreName"].stringValue
-                let trackPrice = i.1["trackPrice"].doubleValue
-                let id = i.1["trackId"].intValue
-                
-                DispatchQueue.main.async {
-                    self.datas.append(DataType(id: id, artistName: artistName, trackName: trackName, releaseDate: releaseDate, primaryGenreName: primaryGenreName, trackPrice: trackPrice))
-                }
-            }
-        }.resume()
-    }
+    
 }
